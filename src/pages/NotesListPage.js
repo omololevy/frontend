21763b123198315fa111aba00ef6b62ee2008d59
omololevy <cotechlevy@gmail.com>
2023-04-03 09:54:38 +0300

    the api url

--- src/pages/NotesListPage.js
+++ src/pages/NotesListPage.js
@@ -12,7 +12,7 @@ const NotesListPage = () => {
     }, [])
 
     let getNotes = async () => {
-        let response = await fetch('https://cotech-note-app-backend.onrender.com/api/notes/')
+        let response = await fetch('http://127.0.0.1:8000/api/notes/')
 
         let data = await response.json()
         // console.log(data);
